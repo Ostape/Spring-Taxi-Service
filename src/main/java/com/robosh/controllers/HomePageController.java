c235618ea0199a530874ceedf725cbcef1fa189a
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-08-17 01:18:06 +0300

    delete reduant info

--- src/main/java/com/robosh/controllers/HomePageController.java
+++ src/main/java/com/robosh/controllers/HomePageController.java
@@ -10,21 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomePageController {
 
-    @Autowired
-    public AddressRepository addressRepository;
-
-    @Autowired
-    public DriverRepository driverRepository;
-
-    @Autowired
-    public OrderRepository orderRepository;
-
     @GetMapping("/taxi-kyiv")
     public String homePage(){
-        System.out.println(addressRepository.findByIdAddress(1L));
-        System.out.println(driverRepository.findById(1L).get());
-
-        System.out.println(orderRepository.findById(6L).get());
         return "home_page";
     }
 }
