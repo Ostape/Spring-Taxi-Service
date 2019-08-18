26e002a9b8348446e24440ea3cc1bec87187885f
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-08-19 02:51:44 +0300

    added password validation from xss atack

--- src/main/java/com/robosh/model/customAnnotations/PasswordMatchesValidator.java
+++ src/main/java/com/robosh/model/customAnnotations/PasswordMatchesValidator.java
@@ -5,9 +5,15 @@ import com.robosh.model.customAnnotations.annotations.PasswordMatches;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PasswordMatchesValidator
         implements ConstraintValidator<PasswordMatches, Object> {
+
+    private Pattern pattern;
+    private Matcher matcher;
+    private static final String CORRECT_PASSWORD = "[a-zA-Z0-9]{5,20}";
 
     @Override
     public void initialize(PasswordMatches constraintAnnotation) {
@@ -16,6 +22,15 @@ public class PasswordMatchesValidator
     @Override
     public boolean isValid(Object obj, ConstraintValidatorContext context) {
         RegistrationClientDto clientDto = (RegistrationClientDto) obj;
-        return clientDto.getPassword().equals(clientDto.getPassword_repeat());
+        if (clientDto.getPassword().equals(clientDto.getPassword_repeat())) {
+            return checkInputPassword(clientDto.getPassword());
+        }
+        return false;
+    }
+
+    private boolean checkInputPassword(String password) {
+        pattern = Pattern.compile(CORRECT_PASSWORD);
+        matcher = pattern.matcher(password);
+        return matcher.matches();
     }
 }
