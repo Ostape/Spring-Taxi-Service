5fb2a9396efdbffa7f20490f434af41cbcb5cc42
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-08-19 06:02:58 +0300

    added findByPhoneNumber in DriverRepository

--- src/main/java/com/robosh/repository/DriverRepository.java
+++ src/main/java/com/robosh/repository/DriverRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 //todo integer -> long
 public interface DriverRepository extends JpaRepository<Driver, Long> {
+    Driver findByPhoneNumberAndPassword(String phoneNumber, String password);
+    Driver findByPhoneNumber(String phoneNumber);
 }
