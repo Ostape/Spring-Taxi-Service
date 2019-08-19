9ffdf4382805130851bc43ddfb70bb8020ce4b3a
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-08-19 07:00:11 +0300

    added addresses to order page

--- src/main/java/com/robosh/controllers/TaxiOrderController.java
+++ src/main/java/com/robosh/controllers/TaxiOrderController.java
@@ -1,6 +1,10 @@
 package com.robosh.controllers;
 
+import com.robosh.service.AddressService;
+import com.robosh.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/taxi-kyiv/client-account")
 public class TaxiOrderController {
+
+    @Autowired
+    private final AddressService addressService;
+
+    public TaxiOrderController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+
     @GetMapping("/making-order")
-    public String makingTaxiOrder(){
+    public String makingTaxiOrder(Model model){
+        model.addAttribute("addresses", addressService.getAllAddresses());
         return "taxi_order";
     }
 
---
+++ src/main/java/com/robosh/service/AddressService.java
@@ -0,0 +1,22 @@
+package com.robosh.service;
+
+import com.robosh.model.entities.Address;
+import com.robosh.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressService {
+    @Autowired
+    private final AddressRepository addressRepository;
+
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    public List<Address> getAllAddresses(){
+        return addressRepository.findAll();
+    }
+}
