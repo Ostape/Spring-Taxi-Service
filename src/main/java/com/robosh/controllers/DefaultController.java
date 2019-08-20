9255e68936c01b366eb47f592f8901713911e658
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-08-21 02:58:47 +0300

    fixed bug when client login and redirected him to driver-account page

--- src/main/java/com/robosh/controllers/DefaultController.java
+++ src/main/java/com/robosh/controllers/DefaultController.java
@@ -1,15 +1,25 @@
 package com.robosh.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.HashSet;
 
 @Controller
 public class DefaultController {
     @GetMapping("/default")
     public String defaultAfterLogin(HttpServletRequest request) {
-        if (request.isUserInRole("ROLE_CLIENT")) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Collection<? extends GrantedAuthority> authorities = new HashSet<>(auth.getAuthorities());
+        boolean isClient = authorities.contains(new SimpleGrantedAuthority("CLIENT"));
+
+        if (isClient) {
             return "redirect:/taxi-kyiv/client-account/";
         }
         return "redirect:/taxi-kyiv/driver-account";
